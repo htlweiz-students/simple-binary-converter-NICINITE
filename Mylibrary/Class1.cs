d45294e017400340bd41d0fb5229ff03fdfaf1ad
Student <foobar@sr.htlweiz.at>
2025-12-14 15:11:13 +0100

    WP_finished

---
+++ Mylibrary/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Mylibrary;
+
+public static class Binary
+{
+    /// <summary>
+    /// Converts an input string with only binary-numbers (0 and 1) 
+    /// to a dezimal number! If there is a other number than 0 or 1, an Error will
+    /// be return back.
+    /// </summary>
+    /// <param name="binary_code">Input string</param>
+    /// <param name="sum"> Is the sum of all added binary numbers in dezimal</param>
+    /// <param name="power"> To calculate the the power of 2 foreach binary-number</param>
+    
+    public static double Todezimal(string? binary_code) {
+        double sum = 0;
+        int power = 0;
+
+        if (string.IsNullOrEmpty(binary_code)) {
+            throw new ArgumentException("Error: Input is empty");
+        }
+
+        for(int count = binary_code.Length-1; count >= 0; count--) {
+            if(binary_code[count] == '1') {
+                sum += Math.Pow(2, power);
+            } else if (binary_code[count] != '0') {
+                throw new ArgumentException("Error: invalid binary number!");
+            }
+            power++;
+        }
+
+        return sum;
+    } 
+}
---
+++ Programm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mylibrary;
+
+namespace Programm
+{
+    class Program{
+        public static int Main() {
+            Console.WriteLine("___Binary-to-dezimal-converter___");
+            Console.Write("Please enter a binanry code: ");
+            string? binary_code = Console.ReadLine();
+
+            double result = Binary.Todezimal(binary_code);
+            Console.WriteLine(result);
+            return 0;
+        }
+    }
+}
